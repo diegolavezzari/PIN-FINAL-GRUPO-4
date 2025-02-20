13ce27c1efbe315c5448294a0eabe3d660cfcaca
Diego <diego.lavezzari@gmail.com>
2025-02-20 01:05:03 -0300

    fasdfasd

--- main.tf
+++ main.tf
@@ -11,22 +11,3 @@ resource "aws_instance" "monitoring" {
   }
 }
 
-# Habilitar versionado en el bucket
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Configurar cifrado del lado del servidor (SSE)
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
