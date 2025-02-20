4d9b9efb9581ff0478ffa923a2b670341e6940a7
Diego <diego.lavezzari@gmail.com>
2025-02-20 00:23:19 -0300

    asfdasd

---
+++ backend.tfvars
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = var.bucket_name
+    key    = "terraform.tfstate"
+    region = var.region
+    encrypt = true
+  }
+}
--- main.tf
+++ main.tf
@@ -1,25 +1,49 @@
-# Configuración del Backend para almacenar el estado en S3
-terraform {
-  backend "s3" {
-    bucket = "mi-bucket-terraform-state-12345"   # Nombre del bucket S3
-    key    = "terraform.tfstate"                 # Nombre del archivo de estado dentro del bucket
-    region = "us-east-1"                         # Región de AWS donde se encuentra el bucket S3
-    encrypt = true                              # Activar cifrado para el archivo de estado
-    acl     = "bucket-owner-full-control"        # Permite control total al propietario del bucket
-  }
-}
-
-# Crear y lanzar una instancia webserver en AWS
-resource "aws_instance" "webserver" {
-  ami                         = "ami-0866a3c8686eaeeba"   # ID de la AMI
-  instance_type               = "t2.micro"                # Tipo de instancia EC2
-  #key_name                    = app-ssh-key 
+resource "aws_instance" "monitoring" {
+  ami                         = "ami-0866a3c8686eaeeba"  # ID de la AMI
+  instance_type               = "t2.micro"               # Tipo de instancia EC2
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
-  user_data                   = "${file("Contenedores.sh")}"
+  user_data                   = file("metrics.sh")  # Ejecutar el script al lanzar la instancia
 
   tags = {
-    Name = "webserver"
+    Name = var.instance_name
+  }
+}
+
+# Crear el bucket S3
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  force_destroy = true
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+# Configurar ACL (permisos) del bucket
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
+}
+
+# Habilitar versionado en el bucket
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Configurar cifrado del lado del servidor (SSE)
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
