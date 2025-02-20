59c4b129548064e99a2cbeb75b5039fbd55c3d7f
Diego <diego.lavezzari@gmail.com>
2025-02-20 00:49:04 -0300

    safsdf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "monitoring" {
-  ami                         = "ami-0866a3c8686eaeeba"  # ID de la AMI
-  instance_type               = "t2.micro"               # Tipo de instancia EC2
+  ami                         = var.ami_id  # ID de la AMI
+  instance_type               = var.instance_type               # Tipo de instancia EC2
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
