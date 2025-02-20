87fc052227466ee14a85e79d5c66152510493594
Diego <diego.lavezzari@gmail.com>
2025-02-20 01:57:34 -0300

    clave privada

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Configuración del Backend para almacenar el estado en S3
 terraform {
   backend "s3" {
-    bucket = "mi-bucket-terraform-state-12345"   # Nombre del bucket S3
-    key    = "terraform.tfstate"                 # Nombre del archivo de estado dentro del bucket
-    region = "us-east-1"                         # Región de AWS donde se encuentra el bucket S3
+    bucket  = var.bucket_name   # Nombre del bucket S3
+    key     = "terraform.tfstate"                 # Nombre del archivo de estado dentro del bucket
+    region  = "us-east-1"                         # Región de AWS donde se encuentra el bucket S3
     encrypt = true                              # Activar cifrado para el archivo de estado
     acl     = "bucket-owner-full-control"        # Permite control total al propietario del bucket
   }
@@ -11,7 +11,7 @@ terraform {
 resource "aws_instance" "monitoring" {
   ami                         = var.ami_id  # ID de la AMI
   instance_type               = var.instance_type               # Tipo de instancia EC2
-  key_name                    = Miclaveprivada
+  key_name                    = "Miclaveprivada"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
