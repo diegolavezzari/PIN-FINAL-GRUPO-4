3449899382116919df5c47e6ea6c4e9db622fab9
Diego <diego.lavezzari@gmail.com>
2025-02-21 01:35:17 -0300

    Se corrige network.tf

--- network.tf
+++ network.tf
@@ -3,21 +3,48 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-
   tags = {
     Name = "main-vpc"
   }
 }
 
+# Crear el Internet Gateway (IGW)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Crear la Tabla de Rutas
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0" # Ruta predeterminada para Internet
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "main-route-table"
+  }
+}
+
 # Crear la Subnet
 resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.subnet_cidr
-  availability_zone = var.availability_zone
-
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true # Hacer que la subnet sea pública
   tags = {
     Name = "webserver-subnet"
   }
+}
+
+# Asociar la Tabla de Rutas con la Subnet
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
 }
 
 # Crear el Security Group
@@ -39,7 +66,7 @@ resource "aws_security_group" "sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # ⚠️ Cambiar en producción por una IP específica
+    cidr_blocks = ["0.0.0.0/0"] # ⚠️ Cambiar en producción por una IP específica
   }
 
   # Permitir salida de la instancia (Todo el tráfico)
