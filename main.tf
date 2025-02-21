b7fa7e1391786a9e0e45773e4dac4a1349a9efbc
Diego <diego.lavezzari@gmail.com>
2025-02-21 00:31:21 -0300

    Se ponen nombre estaticos en el main.

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 # Configuración del Backend para almacenar el estado en S3
 terraform {
   backend "s3" {
-    bucket  = var.bucket_name   # Nombre del bucket S3
-    key     = "terraform.tfstate"                 # Nombre del archivo de estado dentro del bucket
-    region  = var.region                         # Región de AWS donde se encuentra el bucket S3
-    encrypt = true                              # Activar cifrado para el archivo de estado
-    acl     = "bucket-owner-full-control"        # Permite control total al propietario del bucket
+    bucket  = "terraform-state"   # Nombre del bucket S3 (valor estático)
+    key     = "terraform.tfstate" # Nombre del archivo de estado dentro del bucket
+    region  = "us-east-1"         # Región de AWS donde se encuentra el bucket S3 (valor estático)
+    encrypt = true                # Activar cifrado para el archivo de estado
   }
 }
 # Crear la instancia EC2
