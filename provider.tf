e356bc5155ae966deaa885161d92a28ed35b8f42
Diego <diego.lavezzari@gmail.com>
2025-02-21 00:59:31 -0300

    se crea bucket desde deploy

--- provider.tf
+++ provider.tf
@@ -1,3 +1,20 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1" # Asegúrate de que la región sea us-east-1
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name # Nombre del bucket
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
