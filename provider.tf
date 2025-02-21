23ba4b88b24492976a225c787d46663ab1c74e50
Diego <diego.lavezzari@gmail.com>
2025-02-21 01:02:43 -0300

    se borra la creacion de bucket en provider.

--- provider.tf
+++ provider.tf
@@ -1,20 +1,3 @@
 provider "aws" {
   region = "us-east-1" # Asegúrate de que la región sea us-east-1
 }
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name # Nombre del bucket
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
